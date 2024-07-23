be1caf75834ed14b9ab8bb75160701ec8429a7d7
Alvaro Laserna <alvarolaserna@MacBook-Pro-2.local>
2024-07-23 13:05:15 +0300

    add more documentation and examples

--- examples/playwrigth-ts/README.md
+++ examples/playwrigth-ts/README.md
@@ -4,36 +4,42 @@
 
 ## Install
 
+move to playwright folder:
+
+    cd examples/playwrigth-ts/
+
+
 - first of all, install all the necessary depenencies for Playwright
     - npx playwright install 
-- Also install all the dependencies for cv_pom and run server:
-    - git clone https://github.com/testdevlab/cv_pom.git
-    - cd cv_pom
-    - python -m pip install requirements.txt
+  - Also install all the dependencies for cv_pom and run server:
+      - git clone https://github.com/testdevlab/cv_pom.git
+      - cd cv_pom
+      - python -m pip install requirements.txt
 
-<pre>
-# Setup virtual environment and activate it
-python -m pip install --upgrade pip
 
-python -m venv venv
-. venv/bin/activate
-python -m pip install --upgrade pip
+    # Setup virtual environment and activate it
+    python -m pip install --upgrade pip
 
-# Install requirements
-python -m pip install -r requirements.txt
-</pre>
+    python -m venv venv
+    . venv/bin/activate
+    python -m pip install --upgrade pip
+
+    # Install requirements
+    python -m pip install -r requirements.txt
 
 ## Run cases
 
 First you will have to run the cv_sdk server:
 
-<pre>
-cd cv_pom/
-python server.py --model ../../../resources/best.pt
-</pre>
+    cd cv_pom/
+    python server.py --model ../../../resources/best.pt
+
 
 in a different terminal run the playwright test:
 
-<pre>
-npx playwright test
-</pre>
+    npx playwright test
+
+or headed:
+
+    npx playwright test --headed
+
--- examples/playwrigth-ts/tests/example.spec.ts
+++ examples/playwrigth-ts/tests/example.spec.ts
@@ -1,32 +1,37 @@
 import { test, request } from '@playwright/test';
 
 test('CV pom test', async ({ page }) => {
-  await page.goto('https://flutter-gallery-archive.web.app/');
+  await page.goto('https://testdevlab.com');
 
-  // Click cookies.
-  await page.locator('#cookie-consent').click();
 
-  console.log(await hasLabel(page, 'rally-main'))
-  console.log(await hasLabel(page, 'shrine-main'))
-  var rally_button = await hasLabel(page, 'rally-main')
-  await page.mouse.click(rally_button.center[0], rally_button.center[1])
-  var login_button = await hasLabel(page, 'rally-login-btn')
-  await page.mouse.click(login_button.center[0], login_button.center[1])
+//         page.element({"text": {"value": "Contact us", "case_sensitive": False}}).click()
+//         page.element({"text": "Accept"}).click()
+//         print(cv_pom_driver.get_page().elements(None))
+//         page = cv_pom_driver.get_page()
+//         page.element({"text": {"value": "Full Name", "case_sensitive": False}}).send_keys("John Doe")
+//
+//         page.element({"text": {"value": "Business E-mail", "case_sensitive": False, "contains": True}}).send_keys("johndoe@example.com")
+//         page.element({"text": {"value": "Message", "case_sensitive": False, "contains": True}}).send_keys("This is a test message")
+  console.log(await parseQuery(page, {"text": {"value": "Contact us", "case_sensitive": false}}))
+  console.log(await parseQuery(page, {"text": "Accept"}))
+
+  var contact_us = await parseQuery(page, {"text": {"value": "Contact us", "case_sensitive": false}})
+  await page.mouse.click(contact_us.center[0], contact_us.center[1])
+  var accept_btn = await parseQuery(page, {"text": "Accept"})
+  await page.mouse.click(accept_btn.center[0], accept_btn.center[1])
 });
 
 
 // HELPERS FOR CV_POM
 
-async function hasLabel(page: any, targetLabel: string): Promise<any> {
+async function parseQuery(page: any, query: object): Promise<any> {
   let i: number = 1;
   while (i < 10) {
     const buffer = await page.screenshot();
-    var query: object = { "label": targetLabel }
+
     var response = await getCVPOM(buffer.toString('base64'), query)
     for (const obj of response) {
-      if (obj.label === targetLabel) {
         return obj;
-      }
     }
     i++
   }
@@ -39,7 +44,7 @@ async function getCVPOM(filedata: string, query: object): Promise<any> {
   const context = await request.newContext();
   
   var raw = {
-    "ocr": false,
+    "ocr": {'paragraph': false},
     "image_base64": filedata,
     "query": query
   };
